fa0f9d7f72e415ce7cb7dc596b04fe75b6e2c876
garethmullins <gareth.mullins@my.jcu.edu.au>
2016-09-01 19:22:56 +1000

    Reposition the dictionary layout

--- AllStateNames.py
+++ AllStateNames.py
@@ -6,7 +6,7 @@ __author__ = 'Gareth'
 
 def main():
     STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
-               "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+                   "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
     for short_state_name in STATE_NAMES:
         print("{:<4} is  {}".format(short_state_name, STATE_NAMES.get(short_state_name)))
 
