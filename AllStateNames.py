b7908965c3bbc9a6137558f47d87f10bef15617d
garethmullins <gareth.mullins@my.jcu.edu.au>
2016-09-01 19:22:14 +1000

    Code a program that has a dictionary and prints the data it contains.

---
+++ AllStateNames.py
@@ -0,0 +1,15 @@
+"""
+Print all State names and their abbreviations
+"""
+__author__ = 'Gareth'
+
+
+def main():
+    STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+               "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+    for short_state_name in STATE_NAMES:
+        print("{:<4} is  {}".format(short_state_name, STATE_NAMES.get(short_state_name)))
+
+
+if __name__ == "__main__":
+    main()
